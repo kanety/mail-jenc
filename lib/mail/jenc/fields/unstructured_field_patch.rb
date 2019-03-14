bec9352acb14bef398e4af5180fca3c23712f011
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2019-03-14 09:22:50 +0900

    Fix for copy of headers

--- lib/mail/jenc/fields/unstructured_field_patch.rb
+++ lib/mail/jenc/fields/unstructured_field_patch.rb
@@ -3,12 +3,14 @@ module Mail
     module UnstructuredFieldPatch
       def initialize(name, value, charset = nil)
         if Jenc.enabled?
-          if value.is_a?(String) && !value.ascii_only? && value.encoding == Encoding::UTF_8 && charset && charset != 'utf-8'
-            value = Mail::Encodings.b_value_encode(
-              Mail::Encodings.transcode_charset(value, value.encoding, charset)
-            )
-          elsif value.is_a?(String) && value.ascii_only?
-            charset = 'us-ascii'
+          if value.is_a?(String) && value.encoding == Encoding::UTF_8 && charset && charset != 'utf-8'
+            if !value.ascii_only?
+              value = Mail::Encodings.b_value_encode(
+                Mail::Encodings.transcode_charset(value, value.encoding, charset)
+              )
+            elsif value !~ Mail::Encodings::ENCODED_VALUE
+              charset = 'us-ascii'
+            end
           end
         end
         super
--- spec/mail/jenc/header_spec.rb
+++ spec/mail/jenc/header_spec.rb
@@ -6,21 +6,35 @@ RSpec.describe Mail::Jenc do
 
     let(:mail) do
       mail = Mail.new(charset: 'iso-2022-jp')
-      mail.header['X-Sender'] = '差出人 <user1@example.com>'
+      mail.from '差出人 <user1@example.com>'
+      mail.to '宛先 <user2@example.com>'
+      mail.subject 'テストメールの件名'
+      mail.header['X-Test'] = 'テストヘッダ'
       mail.header['X-Mailer'] = 'Mailer'
       mail
     end
 
     it 'encodes unstructured field' do
-      expect(mail.header['X-Sender'].value).to include(b_encode('差出人', 'iso-2022-jp'))
-    end
-
-    it 'does not encode unstructured field with us-ascii' do
+      expect(mail.header[:subject].value).to include(b_encode('テストメールの件名', 'iso-2022-jp'))
+      expect(mail.header['X-Test'].value).to include(b_encode('テストヘッダ', 'iso-2022-jp'))
       expect(mail.header['X-Mailer'].value).to include('Mailer')
     end
 
     it 'builds ascii mail' do
       expect(mail.encoded.ascii_only?).to eq(true)
+    end
+
+    it 'copies headers' do
+      copy = Mail.new(charset: 'iso-2022-jp')
+      mail.header.fields.each do |field|
+        copy.header[field.name] = field.value
+      end
+      expect(copy.header[:from].value).to include(b_encode('差出人', 'iso-2022-jp'))
+      expect(copy.header[:to].value).to include(b_encode('宛先', 'iso-2022-jp'))
+      expect(copy.header[:subject].value).to include(b_encode('テストメールの件名', 'iso-2022-jp'))
+      expect(copy.header['X-Test'].value).to include(b_encode('テストヘッダ', 'iso-2022-jp'))
+      expect(copy.header['X-Mailer'].value).to include('Mailer')
+      expect(copy.encoded.ascii_only?).to eq(true)
     end
   end
 end
