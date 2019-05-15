1e328f04cf430ab55af95ee13ec4e0983727d6e7
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2019-05-15 11:46:32 +0900

    Remove rfc2231 key from options hash

--- spec/mail/jenc/attachment_spec.rb
+++ spec/mail/jenc/attachment_spec.rb
@@ -7,27 +7,33 @@ RSpec.describe Mail::Jenc do
 
     it 'uses rfc2231 format if true' do
       mail = Mail.new(charset: 'utf-8') do
+        body 'test'
         add_file content: '添付ファイルの内容', filename: '添付ファイル.txt', rfc2231: true
       end
-      field = mail.parts[0][:content_disposition]
-      expect(field.parameters['filename*0*']).to include("utf-8''#{url_encode('添付ファイル')}")
+      encoded = "utf-8''#{url_encode('添付ファイル')}"
+      expect(mail.parts[1][:content_disposition].parameters['filename*0*']).to include(encoded)
+      expect(mail.parts[1].encoded).to include(encoded)
     end
 
     it 'does not use rfc2231 format if false' do
       mail = Mail.new(charset: 'utf-8') do
+        body 'test'
         add_file content: '添付ファイルの内容', filename: '添付ファイル.txt', rfc2231: false
       end
-      field = mail.parts[0][:content_disposition]
-      expect(field.parameters['filename']).to include(b_encode('添付ファイル'))
+      encoded = b_encode('添付ファイル')
+      expect(mail.parts[1][:content_disposition].parameters['filename']).to include(encoded)
+      expect(mail.parts[1].encoded).to include(encoded)
     end
 
     it 'refers global option for rfc2231' do
       Mail::Jenc.rfc2231 = true
       mail = Mail.new(charset: 'utf-8') do
+        body 'test'
         add_file content: '添付ファイルの内容', filename: '添付ファイル.txt'
       end
-      field = mail.parts[0][:content_disposition]
-      expect(field.parameters['filename*0*']).to include("utf-8''#{url_encode('添付ファイル')}")
+      encoded = "utf-8''#{url_encode('添付ファイル')}"
+      expect(mail.parts[1][:content_disposition].parameters['filename*0*']).to include(encoded)
+      expect(mail.parts[1].encoded).to include(encoded)
     end
   end
 end
